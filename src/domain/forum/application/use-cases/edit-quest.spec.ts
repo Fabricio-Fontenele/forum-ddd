ae7721a97ca5bbe2c0c5a805672b05c1560005db
Fabricio-Fontenele <vieirafabricio081@gmail.com>
2026-03-17 22:52:48 -0300

    feat: implement EditQuestion use case and add save method to QuestionsRepository

---
+++ src/domain/forum/application/use-cases/edit-quest.spec.ts
@@ -0,0 +1,62 @@
+import { beforeEach, expect, it, test } from 'vitest'
+import { describe } from 'node:test'
+import { InMemoryQuestionsRepository } from '@/repositories/in-memory-questions-repository'
+import { EditQuestionUseCase } from './edit-question'
+import { makeQuestion } from '@/factories/make-question'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { title } from 'node:process'
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let sut: EditQuestionUseCase
+
+describe('Edit question', () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    sut = new EditQuestionUseCase(inMemoryQuestionsRepository)
+  })
+
+  it('Should be able to edit a question', async () => {
+    const newQuestion = makeQuestion(
+      {
+        authorId: new UniqueEntityId('author-1'),
+      },
+      new UniqueEntityId('question-1'),
+    )
+
+    await inMemoryQuestionsRepository.create(newQuestion)
+
+    await sut.execute({
+      questionId: newQuestion.id.toValue(),
+      authorId: 'author-1',
+      title: 'Pergunta teste',
+      content: 'Conteúdo teste',
+    })
+
+    expect(inMemoryQuestionsRepository.items[0]).toMatchObject({
+      title: 'Pergunta teste',
+      content: 'Conteúdo teste',
+    })
+  })
+
+  it('Should not be able to edit a question from another user', async () => {
+    const newQuestion = makeQuestion(
+      {
+        authorId: new UniqueEntityId('author-2'),
+      },
+      new UniqueEntityId('question-1'),
+    )
+
+    await inMemoryQuestionsRepository.create(newQuestion)
+
+    expect(async () => {
+      await sut.execute({
+        questionId: newQuestion.id.toValue(),
+        authorId: 'author-1',
+        title: 'Pergunta teste',
+        content: 'Conteúdo test',
+      })
+    }).rejects.instanceOf(Error)
+
+    expect(inMemoryQuestionsRepository.items).toHaveLength(1)
+  })
+})
