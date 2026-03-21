19e89384ec6751c21de0f0ab3d11f50db4269cb5
Fabricio-Fontenele <vieirafabricio081@gmail.com>
2026-03-20 23:21:14 -0300

    feat: implement ChooseQuestionBestAnswer use case and add tests for best answer selection

---
+++ src/domain/forum/application/use-cases/choose-question-best-answer.spec.ts
@@ -0,0 +1,60 @@
+import { beforeEach, expect, it, describe } from 'vitest'
+import { InMemoryQuestionsRepository } from '@/repositories/in-memory-questions-repository'
+import { makeQuestion } from '@/factories/make-question'
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { InMemoryAnswersRepository } from '@/repositories/in-memory-answers-repository'
+import { ChooseQuestionBestAnswerUseCase } from './choose-question-best-answer'
+import { makeAnswer } from '@/factories/make-answer'
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let inMemoryAnswersRepository: InMemoryAnswersRepository
+let sut: ChooseQuestionBestAnswerUseCase
+
+describe('Choose question best answer', () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    inMemoryAnswersRepository = new InMemoryAnswersRepository()
+    sut = new ChooseQuestionBestAnswerUseCase(
+      inMemoryQuestionsRepository,
+      inMemoryAnswersRepository,
+    )
+  })
+
+  it('Should be able to choose the best answer for a question', async () => {
+    const question = makeQuestion()
+    const answer = makeAnswer({
+      questionId: question.id,
+    })
+
+    await inMemoryQuestionsRepository.create(question)
+    await inMemoryAnswersRepository.create(answer)
+
+    await sut.execute({
+      answerId: answer.id.toString(),
+      authorId: question.authorId.toString(),
+    })
+
+    expect(inMemoryQuestionsRepository.items[0].bestAnswerId).toEqual(answer.id)
+  })
+
+  it('Should not be able to choose the best answer for a question from another user', async () => {
+    const question = makeQuestion({
+      authorId: new UniqueEntityId('author-1'),
+    })
+    const answer = makeAnswer({
+      questionId: question.id,
+    })
+
+    await inMemoryQuestionsRepository.create(question)
+    await inMemoryAnswersRepository.create(answer)
+
+    expect(async () => {
+      await sut.execute({
+        answerId: answer.id.toString(),
+        authorId: 'author-2',
+      })
+    }).rejects.instanceOf(Error)
+
+    expect(inMemoryQuestionsRepository.items).toHaveLength(1)
+  })
+})
